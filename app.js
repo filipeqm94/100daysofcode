809dbaa70f1eb2a221120b569b0380946cd21c5f
Filipe <queiroz.filipe94@gmail.com>
2021-04-19 15:51:53 -0400

    Add most functionallity to the Blog Website. Will revisit this in the future and publish it.

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const ejs = require("ejs");
+const _ = require("lodash");
+
+const app = express();
 
 //VARIABLES
 const homeText =
@@ -8,16 +11,20 @@ const homeText =
 const aboutPage = "about me page";
 
 const contactPage = "contact me page";
-const app = express();
+
+const posts = [];
 
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+//GET requests
+
 app.get("/", (req, res) => {
   res.render("home", {
     text: homeText,
+    posts,
   });
 });
 
@@ -31,6 +38,40 @@ app.get("/contact", (req, res) => {
   res.render("contact", {
     text: contactPage,
   });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.get("/post/:postName", (req, res) => {
+  const postName = _.lowerCase(req.params.postName);
+
+  const postTitles = [];
+
+  posts.forEach((post) => {
+    postTitles.push(_.lowerCase(post.title));
+  });
+
+  const n = postTitles.indexOf(postName);
+
+  res.render("post", {
+    title: posts[n].title,
+    body: posts[n].body,
+  });
+});
+
+// POST requests
+
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.blogPostTitle,
+    body: req.body.blogPostBody,
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
